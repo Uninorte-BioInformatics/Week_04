6fe0279d3bca0c01b347c20a901542039353b6f4
camaranto <42853456+camaranto@users.noreply.github.com>
2022-02-24 19:55:58 -0500

    Update punto_4.R

--- punto_4.R
+++ punto_4.R
@@ -2,6 +2,11 @@ library("DataQualityDashboard")
 
 
 DataQualityDashboard::viewDqDashboard(
-  jsonPath = file.path(getwd(), "data", "mimic_iv_demo", "json", "results_MIMICIVdemo210426.json"))
+  jsonPath = file.path(
+    getwd(), 
+    "data", 
+    "mimic_iv_demo", 
+    "json", 
+    "results_MIMICIVdemo210426.json"
+  )
 )
-
